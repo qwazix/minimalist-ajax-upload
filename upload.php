fa8fefca3659b972077a478ae2a4a460481f56b8
Michael Demetriou <accounts@qzx.gr>
2019-07-09 16:22:55 +0300

    WIP: Add drag and drop upload capability (breaks past behavior due to changes in the php part. Need to reconcile)

--- upload.php
+++ upload.php
@@ -1,4 +1,10 @@
 <?php
 
-print_r($_FILES);
-print_r($_POST);
+$response = array_pop($_FILES);
+
+$response["url"] = "http://snap.as".$response["tmp_name"]."/".$response["name"];
+
+echo json_encode($response);
+
+// print_r($_FILES);
+// print_r($_POST);
