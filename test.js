0a10564dc8302ec65bc6ec3865ab38a5869de29f
Michael Demetriou <accounts@qzx.gr>
2019-07-10 12:54:51 +0300

    Now the test works with regular expressions

--- test.js
+++ test.js
@@ -3,20 +3,21 @@
 // oldstring = 'this is some text rest';
 // newstring = 'this is some text xxxpatrr rest';
 
-oldstring = 'this is some xxxpatrr text rest xxxpattr';
-newstring = 'this is some xxxpatrr text xxxpatrr rest xxxpattr';
+oldstring = 'this is some xxxpratatrr text rest xxxpattr';
+newstring = 'this is some xxxpratatrr text xxxprggatatrr rest xxxpattr';
 
 var index = 0;
 
 do {
     // find first occurence of pattern
-    index = newstring.indexOf('xxxpatrr', index);
+    var match = newstring.substr(index, newstring.length).match(/xx.*?rr/);
+    index += match.index;
     // take the part of the string from the beginning until the end of the pattern
     // and check if you can find it in the old string
     // if you can go to the next occurence of the pattern until you find one
     // that doesn't exist in the old string.
-    index += 'xxxpatrr'.length;
+    index += match[0].length;
 } while (oldstring.indexOf(newstring.substr(0, index)) != -1);
-res = newstring.substr(0,index-'xxxpatrr'.length) + "[yay]" + newstring.substr(index,newstring.length);
+res = newstring.substr(0,index-match[0].length) + "[yay]" + newstring.substr(index,newstring.length);
 
 console.log(res);
