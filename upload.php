df28df2e869d081f3097cda64d0a54abdbc865a8
Michael Demetriou <accounts@qzx.gr>
2019-07-14 14:40:36 +0300

    Add CORS headers to upload.php so that I can test writefreely without having completed a backend system in Go

--- upload.php
+++ upload.php
@@ -1,5 +1,8 @@
 <?php
 
+header("Access-Control-Allow-Origin: *");
+header("Access-Control-Allow-Headers: cache-control, x-file-name, x-file-size, x-requested-with");
+
 $response = array_pop($_FILES);
 
 $response["url"] = "http://snap.as".$response["tmp_name"]."/".$response["name"];
